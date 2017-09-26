efa173fbbd6c212502bda782115aa2b29f41129d
Raul Sanchez <raulsanchez1981@gmail.com>
2017-09-26 09:55:20 +0200

    Swagger and name app added

--- src/main/java/challenge/controllers/HelloController.java
+++ src/main/java/challenge/controllers/HelloController.java
@@ -1,14 +1,36 @@
 package challenge.controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    @RequestMapping(method= RequestMethod.GET, value="/holaGET")
+    public String methodGet(@PathVariable String owner) {
+        return "Holaaaa GET";
     }
 
+    @RequestMapping(method= RequestMethod.POST, value="/HolaPOST")
+    public String methodPost(@RequestBody String prueba) {
+        return "Hola POST";
+    }
+
+    @RequestMapping(method= RequestMethod.PUT, value="/holaPUT")
+    public String methodPUT(@RequestBody String prueba) {
+        return "Hola PUT";
+    }
+
+    @RequestMapping(method= RequestMethod.PATCH, value="/holaPATCH")
+    public String methodPATCH(@RequestBody String prueba) {
+        return "Hola PATCH";
+    }
+
+    @RequestMapping(method= RequestMethod.DELETE, value="/holaDELETE")
+    public void methodDELETE(@RequestHeader String prueba) {
+    }
 }
