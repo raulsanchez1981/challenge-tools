ccecabedebc0bd5305af23e62b78afbf279bff46
Raul Sanchez <raulsanchez1981@gmail.com>
2017-09-26 09:24:43 +0200

    Added Swagger

---
+++ src/main/java/challenge/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package challenge;
+
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket newsApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .groupName("challenge")
+            .apiInfo(apiInfo())
+            .select()
+            .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
+            .paths((PathSelectors.any()))
+            .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("REST Api with Spring Boot, MongoDB and Swagger")
+            .description("Challenge for Tools Team")
+            .contact("Tools Team")
+            .version("1.0")
+            .build();
+    }
+}
