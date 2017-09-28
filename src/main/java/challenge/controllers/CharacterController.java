09b4f3862e95d00b6d027d56bc62e82c470f1beb
Cristian Bou Genoves <cbougeno@usersad.everis.int>
2017-09-28 12:02:03 +0200

    Done Case sensitive on Services

--- src/main/java/challenge/controllers/CharacterController.java
+++ src/main/java/challenge/controllers/CharacterController.java
@@ -1,7 +1,6 @@
 package challenge.controllers;
 
 import challenge.entities.Character;
-import challenge.entities.Character;
 import challenge.services.CharacterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
